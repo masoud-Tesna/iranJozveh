ca2e9e295d6e6b547d675931a57216f433501ef1
Masoud / <53110009+masoud-Tesna@users.noreply.github.com>
2024-03-17 09:06:11 +0330

    fix edit user remove nationalCode and mobile number

--- src/app/dashboard/users/components/save-user-form.js
+++ src/app/dashboard/users/components/save-user-form.js
@@ -1,33 +1,33 @@
-import {Button, Col, Empty, Form, Input, Row, Select, Space, Spin} from 'antd';
-import {useRequest} from '@/utils/useRequest';
-import {useEffect, useMemo, useState} from 'react';
+import { Button, Col, Empty, Form, Input, Row, Select, Space, Spin } from 'antd';
+import { useRequest } from '@/utils/useRequest';
+import { useEffect, useMemo, useState } from 'react';
 import debounce from 'lodash.debounce';
-import {handleCreateAntdZodValidator} from '@/utils/helpers';
-import {NewUserZod} from '@/app/dashboard/users/schema/new-user';
-import {DeleteOutlined} from '@ant-design/icons';
-import {useQueryClient} from '@tanstack/react-query';
+import { handleCreateAntdZodValidator } from '@/utils/helpers';
+import { NewUserZod } from '@/app/dashboard/users/schema/new-user';
+import { DeleteOutlined } from '@ant-design/icons';
+import { useQueryClient } from '@tanstack/react-query';
 import starkString from 'starkstring';
 
-const SaveUserForm = ({handleCloseModal, editUserId, editUserData, setUserToken, handleOpenTokenModal}) => {
-  const [formRef] = Form.useForm();
-  const request = useRequest();
+const SaveUserForm = ({ handleCloseModal, editUserId, editUserData, setUserToken, handleOpenTokenModal }) => {
+  const [ formRef ] = Form.useForm();
+  const request     = useRequest();
   const queryClient = useQueryClient();
   
   const nationalCodeWatch = Form.useWatch('nationalCode', formRef);
   const mobileNumberWatch = Form.useWatch('mobileNumber', formRef);
   
-  const [searchProducts, setSearchProducts] = useState('');
+  const [ searchProducts, setSearchProducts ] = useState('');
   
   const {
-    data: searchedProductsData,
-    isLoading: searchedProductsIsLoading
-  } = request.useQuery({
+          data: searchedProductsData,
+          isLoading: searchedProductsIsLoading
+        } = request.useQuery({
     url: '/api/v1/product',
     params: {
       pageNumber: 1,
       search: searchProducts
     },
-    queryKey: ['products', {pageNumber: 1, search: searchProducts}]
+    queryKey: [ 'products', { pageNumber: 1, search: searchProducts } ]
   });
   
   const searchedProducts = searchedProductsData?.response?.products || [];
@@ -39,15 +39,15 @@ const SaveUserForm = ({handleCloseModal, editUserId, editUserData, setUserToken,
     formRef.setFieldValue('selectedProducts', selectedProducts?.filter(item => item?.value !== productId));
   };
   
-  const {isPending: createCustomerIsLoading, mutateAsync: createCustomerRequest} = request.useMutation({
+  const { isPending: createCustomerIsLoading, mutateAsync: createCustomerRequest } = request.useMutation({
     url: '/api/v1/customer',
-    mutationKey: ['createCustomer']
+    mutationKey: [ 'createCustomer' ]
   });
   
-  const {isPending: updateCustomerIsLoading, mutateAsync: updateCustomerRequest} = request.useMutation({
-    url: `/api/v1/customer/${editUserId}`,
+  const { isPending: updateCustomerIsLoading, mutateAsync: updateCustomerRequest } = request.useMutation({
+    url: `/api/v1/customer/${ editUserId }`,
     method: 'patch',
-    mutationKey: ['updateCustomer']
+    mutationKey: [ 'updateCustomer' ]
   });
   
   const handleOnFinishForm = async () => {
@@ -66,7 +66,7 @@ const SaveUserForm = ({handleCloseModal, editUserId, editUserData, setUserToken,
         setUserToken(res?.response?.token);
       }
       
-      await queryClient.refetchQueries({queryKey: ['users-list']});
+      await queryClient.refetchQueries({ queryKey: [ 'users-list' ] });
       
       if (!editUserId) {
         await handleOpenTokenModal();
@@ -74,8 +74,9 @@ const SaveUserForm = ({handleCloseModal, editUserId, editUserData, setUserToken,
       
       await handleCloseModal();
       
-    } catch (error) {
-      const selectedProductsError = error?.errorFields?.find(item => item?.name[0] === 'selectedProducts');
+    }
+    catch (error) {
+      const selectedProductsError = error?.errorFields?.find(item => item?.name[ 0 ] === 'selectedProducts');
       
       if (selectedProductsError?.errors) {
         formRef.setFields([
@@ -90,163 +91,168 @@ const SaveUserForm = ({handleCloseModal, editUserId, editUserData, setUserToken,
   
   useEffect(() => {
     if (editUserId?.length) {
-      const {products, ...rest} = editUserData;
+      const { products, ...rest } = editUserData;
       formRef.setFieldsValue({
         ...rest,
-        selectedProducts: products?.map(item => ({label: item?.name, value: item?._id}))
+        selectedProducts: products?.map(item => ({ label: item?.name, value: item?._id }))
       });
     }
-  }, [editUserId]);
+  }, [ editUserId ]);
   
   useEffect(() => {
-    formRef.setFields([
-      {
-        name: 'nationalCode',
-        value: starkString(nationalCodeWatch).englishNumber().parseNumber().toString(),
-        errors: []
-      }
-    ]);
-  }, [nationalCodeWatch]);
+    if (nationalCodeWatch?.length) {
+      formRef.setFields([
+        {
+          name: 'nationalCode',
+          value: starkString(nationalCodeWatch).englishNumber().parseNumber().toString(),
+          errors: []
+        }
+      ]);
+    }
+    
+  }, [ nationalCodeWatch ]);
   
   useEffect(() => {
-    formRef.setFields([
-      {
-        name: 'mobileNumber',
-        value: starkString(mobileNumberWatch).englishNumber().parseNumber().toString(),
-        errors: []
-      }
-    ]);
-  }, [mobileNumberWatch]);
+    if (mobileNumberWatch?.length) {
+      formRef.setFields([
+        {
+          name: 'mobileNumber',
+          value: starkString(mobileNumberWatch).englishNumber().parseNumber().toString(),
+          errors: []
+        }
+      ]);
+    }
+  }, [ mobileNumberWatch ]);
   
   return (
-    <Spin spinning={createCustomerIsLoading || updateCustomerIsLoading}>
+    <Spin spinning={ createCustomerIsLoading || updateCustomerIsLoading }>
       <Form
         layout="vertical"
-        form={formRef}
+        form={ formRef }
       >
         <Form.Item
-          name={'selectedProducts'}
-          rules={[handleCreateAntdZodValidator(NewUserZod)]}
+          name={ 'selectedProducts' }
+          rules={ [ handleCreateAntdZodValidator(NewUserZod) ] }
           hidden
         />
         
-        <Row gutter={8}>
-          <Col span={12}>
+        <Row gutter={ 8 }>
+          <Col span={ 12 }>
             <Form.Item
-              name={'fullName'}
-              rules={[handleCreateAntdZodValidator(NewUserZod)]}
+              name={ 'fullName' }
+              rules={ [ handleCreateAntdZodValidator(NewUserZod) ] }
             >
               <Input placeholder="نام و نام خانوادگی" />
             </Form.Item>
           </Col>
           
-          <Col span={6}>
+          <Col span={ 6 }>
             <Form.Item
-              name={'nationalCode'}
-              rules={[handleCreateAntdZodValidator(NewUserZod)]}
+              name={ 'nationalCode' }
+              rules={ [ handleCreateAntdZodValidator(NewUserZod) ] }
             >
-              <Input placeholder="کد ملی" maxLength={10} />
+              <Input placeholder="کد ملی" maxLength={ 10 } />
             </Form.Item>
           </Col>
           
-          <Col span={6}>
+          <Col span={ 6 }>
             <Form.Item
-              name={'mobileNumber'}
-              rules={[handleCreateAntdZodValidator(NewUserZod)]}
+              name={ 'mobileNumber' }
+              rules={ [ handleCreateAntdZodValidator(NewUserZod) ] }
             >
-              <Input placeholder="شماره موبایل" maxLength={11} />
+              <Input placeholder="شماره موبایل" maxLength={ 11 } />
             </Form.Item>
           </Col>
           
-          <Col span={24}>
+          <Col span={ 24 }>
             <Form.Item
-              name={'search'}
+              name={ 'search' }
               label="درسنامه های مجاز برای این کاربر"
             >
               <Select
-                onChange={() => {
+                onChange={ () => {
                   formRef.setFields([
                     {
                       name: 'search',
                       errors: []
                     }
                   ]);
-                }}
-                loading={searchedProductsIsLoading}
-                placeholder={'جستجو درسنامه...'}
+                } }
+                loading={ searchedProductsIsLoading }
+                placeholder={ 'جستجو درسنامه...' }
                 showSearch
-                filterOption={false}
-                notFoundContent={searchedProductsIsLoading ?
-                  <Empty description={<Spin size="small" />} /> :
-                  <Empty />}
-                onSearch={debounceFetcher}
-                options={searchedProducts?.map(searchedProduct => {
+                filterOption={ false }
+                notFoundContent={ searchedProductsIsLoading ?
+                  <Empty description={ <Spin size="small" /> } /> :
+                  <Empty /> }
+                onSearch={ debounceFetcher }
+                options={ searchedProducts?.map(searchedProduct => {
                   return {
                     label: searchedProduct?.name,
                     value: searchedProduct?._id
                   };
-                })}
-                onSelect={(selectedProductId, option) => {
+                }) }
+                onSelect={ (selectedProductId, option) => {
                   const oldData = formRef.getFieldValue('selectedProducts') || [];
                   
                   formRef.setFields([
                     {
                       name: 'selectedProducts',
-                      value: oldData.find(item => item?.value === selectedProductId) ? oldData : [...oldData, option]
+                      value: oldData.find(item => item?.value === selectedProductId) ? oldData : [ ...oldData, option ]
                     }
                   ]);
                   
                   formRef.setFieldValue('search', null);
-                }}
+                } }
               />
             </Form.Item>
           </Col>
           
-          <Col span={24} className="mt-8">
+          <Col span={ 24 } className="mt-8">
             <Form.Item
-              shouldUpdate={(prevValues, curValues) => prevValues?.selectedProducts !== curValues?.selectedProducts}
+              shouldUpdate={ (prevValues, curValues) => prevValues?.selectedProducts !== curValues?.selectedProducts }
               noStyle
             >
-              {({getFieldValue}) => {
+              { ({ getFieldValue }) => {
                 const selectedProducts = getFieldValue('selectedProducts') || [];
                 
                 return (
                   <Row className="text-gray-70 !text-captionMd [&>div]:px-16 [&>div]:h-[56px] [&>div]:leading-[56px] --newUserSelectedCourse">
-                    {selectedProducts?.map(product => (
-                      <Col span={24} key={product?.value}>
+                    { selectedProducts?.map(product => (
+                      <Col span={ 24 } key={ product?.value }>
                         <Row align="middle" justify="space-between">
                           <Col>
                             <span className="--listDot"></span>
-                            {product?.label}
+                            { product?.label }
                           </Col>
                           
                           <Col>
                             <DeleteOutlined
                               className="!text-red-600 cursor-pointer"
-                              onClick={() => handleDeleteProduct(product?.value)}
+                              onClick={ () => handleDeleteProduct(product?.value) }
                             />
                           </Col>
                         </Row>
                       </Col>
-                    ))}
+                    )) }
                   </Row>
                 );
-              }}
+              } }
             </Form.Item>
           </Col>
           
-          <Col span={24} className="pt-64 text-center">
+          <Col span={ 24 } className="pt-64 text-center">
             <Space>
-              <Button className="w-[176px]" onClick={handleCloseModal}>
+              <Button className="w-[176px]" onClick={ handleCloseModal }>
                 لغو
               </Button>
               
               <Button
                 type="primary"
                 className="w-[176px]"
-                onClick={handleOnFinishForm}
+                onClick={ handleOnFinishForm }
               >
-                {editUserId ? 'ویرایش حساب کاربری' : 'ایجاد حساب کاربری'}
+                { editUserId ? 'ویرایش حساب کاربری' : 'ایجاد حساب کاربری' }
               </Button>
             </Space>
           </Col>
